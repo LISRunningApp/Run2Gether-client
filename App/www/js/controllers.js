ea229511607c554e4b2a6289de64658e52230d63
Ferran Plaza <ferran.plaza@e-campus.uab.cat>
2016-05-04 16:05:47 +0200

    Project template mockup from creator

---
+++ App/www/js/controllers.js
@@ -0,0 +1,50 @@
+angular.module('app.controllers', [])
+        
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('registerCtrl', function($scope) {
+
+})
+   
+.controller('runTogetherCtrl', function($scope) {
+
+})
+   
+.controller('inicioCtrl', function($scope) {
+
+})
+   
+.controller('amigosCtrl', function($scope) {
+
+})
+   
+.controller('gruposCtrl', function($scope) {
+
+})
+   
+.controller('historialActividadesCtrl', function($scope) {
+
+})
+   
+.controller('profileCtrl', function($scope) {
+
+})
+   
+.controller('resumenActividadCtrl', function($scope) {
+
+})
+
+.controller('addGrupoCtrl', function($scope) {
+
+})
+
+.controller('addAmigo', function($scope) {
+
+})
+
+.controller('selectAmigos', function($scope) {
+
+})
+ 
---
+++ App/www/js/routes.js
@@ -0,0 +1,95 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/page5',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('register', {
+    url: '/page6',
+    templateUrl: 'templates/register.html',
+    controller: 'registerCtrl'
+  })
+
+  .state('runTogether', {
+    url: '/page7',
+    templateUrl: 'templates/runTogether.html',
+    controller: 'runTogetherCtrl'
+  })
+
+  .state('inicio', {
+    url: '/page8',
+    templateUrl: 'templates/inicio.html',
+    controller: 'inicioCtrl'
+  })
+
+  .state('amigos', {
+    url: '/page9',
+    templateUrl: 'templates/amigos.html',
+    controller: 'amigosCtrl'
+  })
+
+  .state('grupos', {
+    url: '/page11',
+    templateUrl: 'templates/grupos.html',
+    controller: 'gruposCtrl'
+  })
+
+  .state('historialActividades', {
+    url: '/page12',
+    templateUrl: 'templates/historialActividades.html',
+    controller: 'historialActividadesCtrl'
+  })
+
+  .state('profile', {
+    url: '/page10',
+    templateUrl: 'templates/profile.html',
+    controller: 'profileCtrl'
+  })
+
+  .state('resumenActividad', {
+    url: '/page13',
+    templateUrl: 'templates/resumenActividad.html',
+    controller: 'resumenActividadCtrl'
+  })
+
+  .state('addGrupo', {
+    url: '/page14',
+    templateUrl: 'templates/addGrupo.html',
+    controller: 'addGrupoCtrl'
+  })
+
+  .state('addAmigo', {
+    url: '/page15',
+    templateUrl: 'templates/addAmigo.html',
+    controller: 'addAmigoCtrl'
+  })
+
+  .state('selectAmigos', {
+    url: '/page16',
+    templateUrl: 'templates/selectAmigos.html',
+    controller: 'selectAmigosCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page7')
+
+  
+
+});
